ca480d091c72fbd212bb34eeb8041087a359ba51
Exe1 <stankusl@gmail.com>
2016-12-22 08:29:24 +0000

    train

---
+++ README.md
@@ -0,0 +1,11 @@
+# RecipeBook
+
+### Preacquisitions
+* NodeJS : http://nodejs.org (PLEASE NOTE VERSION 4.x)
+* bower : npm install -g bower
+* gulp : npm install -g gulp
+
+### Installation instructions
+* run command ```npm install```
+* run command ```bower install```
+* run command ```gulp```
---
+++ src/app/components/home/home-controller.js
@@ -0,0 +1,48 @@
+(function() {
+    'use strict';
+    angular.module('application').controller('HomeController', function($rootScope, $scope, $log, $q, $state, $stateParams, HomeServices) {
+        self = this;
+        $rootScope.pageTitle = 'Home';
+
+        self.recipes = {};
+        self.recipe = {};
+
+
+        switch ($state.current.name) {
+
+            case 'home':
+
+                HomeServices.getAllRecipes().then(
+                    function(result) {
+                        // console.log(result);
+                        self.recipes = result.data;
+                    },
+                    function(err) {
+                        console.log('Error saving to endpoint: ', err);
+                    });
+                break;
+
+
+
+            case 'recipe':
+
+                HomeServices.getRecipeById($stateParams.recipeId).then(
+                    function(result) {
+                        // console.log(result);
+                        self.recipe = result[0];
+                    },
+                    function(err) {
+                        console.log('Error saving to endpoint: ', err);
+                    });
+                break;
+
+
+
+            default:
+        }
+
+
+
+
+    });
+})();
