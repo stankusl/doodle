b785311fcb7a2abb5528085befa91e3607fa74a0
Luke Stankus <itrash.1990@gmail.com>
2016-12-26 20:20:40 +0000

    save

--- src/app/components/home/home-controller.js
+++ src/app/components/home/home-controller.js
@@ -5,6 +5,30 @@
     function HomeController($rootScope, $scope) {
         self = this;
         $rootScope.pageTitle = 'Home';
+
+
+
+        self.bugObj = function (options) {
+            this.doodleName = options.name || 'unnamed';
+            this.teamName = options.team || 'foo';
+            this.attributes = {
+                speed: options.attributes.speed,
+                strength: options.attributes.strength,
+                intelligence: options.attributes.intelligence
+            };
+            this.getHighestRating = function () {
+                var highestRating = Math.max(this.attributes.speed, Math.max(this.attributes.strength, this.attributes.intelligence));
+                return highestRating;
+            };
+            this.getName = function () {
+                return this.doodleName;
+            };
+            this.formatName = function () {
+                return this.doodleName + ':bug'
+            }
+        }
+
+
     };
 
 
